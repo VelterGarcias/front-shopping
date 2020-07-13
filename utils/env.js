a76e4549b0a7ccd478bf7ff4e497e49a0a9a3f97
VelterGarcias <veltj2@gmail.com>
2020-07-13 10:31:05 -0300

    Front com primeiro build

--- utils/env.js
+++ utils/env.js
@@ -1,4 +1,4 @@
-//const serverUrl = 'https://academiafitnestraining.herokuapp.com'
+//const serverUrl = 'https://back-shopping.herokuapp.com/'
 const serverUrl = 'http://localhost:3333'
 
 export default serverUrl
